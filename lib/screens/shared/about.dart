1472dda8440ce2ca494da8f334471871d02ec0bc
seannarvasa <knightdubster@gmail.com>
2023-04-11 15:35:44 +0800

    Update about page

--- lib/screens/shared/about.dart
+++ lib/screens/shared/about.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 // 🏘️ Local imports
+import '../../core/utils/global.dart';
 import '../../router/navigator/navigation_menu.dart';
 import '../../styles/icons/builder_icons.dart';
 
@@ -12,7 +13,7 @@ class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      minimum: const EdgeInsets.only(top: 130, left: 27, right: 27),
+      minimum: const EdgeInsets.only(top: 0, left: 30, right: 30),
       maintainBottomViewPadding: false,
       child: ConstrainedBox(
         constraints: pageConstraints,
@@ -20,55 +21,35 @@ class AboutPage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Icon(ProjectBuilder.logo, size: 100),
+              const Text(
+                Var.about,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: Var.defaultFont,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Image.asset(
+                Var.appLogo,
+                width: 150,
+                height: 150,
+                fit: BoxFit.cover,
+                alignment: Alignment.center,
+                filterQuality: FilterQuality.high,
+              ),
               const SizedBox(height: 24),
-              Text(
-                'Its an opensource application, with a couple of features. I you like its concept you can checkout its github repo and contribute ',
-                style: Theme.of(context).textTheme.bodyText2!,
+              const Text(
+                Var.aboutBuilder,
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                  fontFamily: Var.defaultFont,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
               ),
               const SizedBox(height: 4),
-              Text(
-                'https://github.com/WalterBanda/ProjectBuilder-app',
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyText2!
-                    .copyWith(color: Theme.of(context).colorScheme.primary),
-              ),
-              TextButton(
-                child: Text(
-                  'Show Licences',
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyText2!
-                      .copyWith(color: Theme.of(context).colorScheme.primary),
-                ),
-                onPressed: () => showAboutDialog(
-                  context: context,
-                  applicationIcon: const Icon(ProjectBuilder.logo, size: 100),
-                  applicationName: "ProjectBuilder",
-                  applicationLegalese: '\u{a9} 2022 Banda Dev',
-                  applicationVersion: '1.0.0',
-                  children: [
-                    const SizedBox(height: 24),
-                    Text(
-                      "ProjectBuilder is a proof-of-concept application that was created "
-                      'for service providers that deliver mechanical services locally.',
-                      style: Theme.of(context).textTheme.bodyText2!,
-                    ),
-                    const SizedBox(height: 4),
-                    Text(
-                      'Its an opensource application, with a couple of features. I you like its concept you can checkout its github repo and contribute ',
-                      style: Theme.of(context).textTheme.bodyText2!,
-                    ),
-                    const SizedBox(height: 4),
-                    Text(
-                      'https://github.com/WalterBanda/ProjectBuilder-app',
-                      style: Theme.of(context).textTheme.bodyText2!.copyWith(
-                          color: Theme.of(context).colorScheme.primary),
-                    )
-                  ],
-                ),
-              ),
             ],
           ),
         ),
